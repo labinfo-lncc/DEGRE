7d187f0497d5891ac8c5894081e59b0011ddcaf7
labinfo-lncc <66260180+labinfo-lncc@users.noreply.github.com>
2022-04-29 19:47:59 -0300

    update doParallel e num_cores

--- R/DEGRE.R
+++ R/DEGRE.R
@@ -1,7 +1,7 @@
 
 
 # DEGRE function
-DEGRE <- function(count_matrix, num_reps, p_value_adjustment = "BH", formula, design_matrix){
+DEGRE <- function(count_matrix, num_reps, p_value_adjustment = "BH", formula, design_matrix, num_cores=2){
   time_start <- Sys.time()
   
   ### Checking steps in the matrices
@@ -186,10 +186,10 @@ DEGRE <- function(count_matrix, num_reps, p_value_adjustment = "BH", formula, de
   names <- as.data.frame(colnames(count_matrix_bip_overdisp))
   colnames(names) <- c("X1")
   
-  cores <- detectCores()
-  no_cores <- round(((round(cores/3))*2.5)-1)
-  cl <- makeCluster(no_cores)
-  registerDoParallel(cl)
+  cores <- doParallel::detectCores()
+  no_cores <- num_cores
+  cl <- doParallel::makeCluster(no_cores)
+  doParallel::registerDoParallel(cl)
   
   calcglmm <- function(i, count_matrix_bip_overdisp) {	
     tmp<-glmmTMB:::Anova.glmmTMB(glmmTMB(as.formula(paste0("count_matrix_bip_overdisp[[i]] ~ ", formula)), 
